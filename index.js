e92c0c223896697c3b7d1bc7948ef5626a74939b
Chaim Reisman <chaim@chaims-mbp.home>
2018-08-12 11:10:55 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function findMinAndRemove(array){
   array.sort()
-  return array[0]
-  array.splice(0, array.length)
+  //return array[0]
+  let smallest = array[0]
+  array.shift()
+  return smallest
 }
 
 function insertionSort(array){
